97640722361e14513d36f1da2cc4fb50a0ee1ec2
MajidZanjani <majidmmz1972@gmail.com>
2023-12-02 00:35:10 +0400

    dev test

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,7 +43,7 @@ export const Header = () => {
               alt="Cinemate Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Cinemate
+              Cinema
             </span>
           </Link>
 
